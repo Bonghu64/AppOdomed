2b000e32e4162db4f3a88a51be08d244b209ef50
Bonghu64 <lcb2024453@est.univalle.edu>
2024-10-29 12:02:19 -0400

    Acomodado de vista y arreglos Usuarios

---
+++ app/src/main/java/com/example/odomedapp/data/User.kt
@@ -0,0 +1,8 @@
+package com.example.odomedapp.data
+
+data class User(
+    val id_usuario: Int,
+    val nombres: String,
+    val email: String
+    // Agrega otros campos según tu tabla de usuarios
+)
